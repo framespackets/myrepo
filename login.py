81af5869d0d994f1304bed02a8d1addd3e7d2317
michaelhanna272 <michaelhanna272@hotmail.co.uk>
2019-12-22 12:12:11 +0000

    pushing login.py

---
+++ login.py
@@ -0,0 +1,15 @@
+##### Login Verification Script #####
+
+user1 = input("Please type your username: ")
+print ("")
+pass1 = input("Please type your password: ")
+print ("")
+
+if user1 == ("framesandpackets"):
+	print ("Welcome framesandpackets")
+	if pass1 == ("password1"):
+		print ("Access Granted!")
+
+	else:
+		print ("Wrong username or password.")
+	
